27e95e8b6ef0ffb8bdedbd006599af9940b4050b
Chicane5 <marshall.gary78@gmail.com>
2016-10-20 00:21:39 -0400

    MV matrix still not working

--- ar.cpp
+++ ar.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <iostream>
+#include <cmath>
 #include <vector>
 #include <GL/glut.h>
 #include <GL/glu.h>
@@ -12,8 +13,7 @@
 using namespace cv;
 using namespace std;
 
-const GLdouble size=0.5;
-const Scalar color = (0, 255, 0);
+const GLdouble size=0.40;
 
 class AR_GL_CV_Tester
 {
@@ -29,6 +29,7 @@ public:
   void handleMarkerTracking();
   void captureProcessWebcam();
   const Mat& getImg() const {return m_img;}
+
 
 private:
   Mat m_img;
@@ -74,13 +75,47 @@ bool AR_GL_CV_Tester::initGL(void)
 {
     //select clearing (background) color
     glClearColor(1.0, 1.0, 1.0, 1.0);
+    //glClearDepth(1.0);
+    //glDepthFunc(GL_LESS);
+    //glEnable(GL_DEPTH_TEST);
+    //glShadeModel(GL_SMOOTH);
+    //glMatrixMode(GL_PROJECTION)
 
     //initialize viewing values
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    //glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    auto imageWidth = 640;
+    auto imageHeight = 480;
+    auto fx = m_intrinsics.at<double>(0,0);
+    auto fy = m_intrinsics.at<double>(1,1);
+    auto centerImageX = m_intrinsics.at<double>(0,2);
+    auto centerImageY = m_intrinsics.at<double>(1,2);
 
-    //enable textures
+    float near = 0.1;
+    float far = 100.0;
+
+    /*
+    glFrustum(-near*centerImageX/(GLfloat)fx,
+              near*(imageWidth-centerImageX)/(GLfloat)fx,
+              near*(centerImageY-imageHeight)/(GLfloat)fy,
+              near*centerImageY/(GLfloat)fy, near, far);
+    */
+
+    auto fovy = 2*atan(0.5*imageHeight/fy)*180/M_PI;
+    auto aspect = (imageWidth*fy)/(imageHeight*fx);
+
+    gluPerspective(fovy,aspect,near,far);
+    glViewport(0,0,imageWidth,imageHeight);
+
+
+
+
+    //gluPerspective(45.0f, 500.0f / 500.0f, 0.1f, 100.0f);
+
+    //glMatrixMode(GL_MODELVIEW);
+
+
     glEnable(GL_TEXTURE_2D);
     glGenTextures(1, &textureID); //videobackground
     glGenTextures(1, &textureID2); //teapot texture
@@ -99,6 +134,7 @@ bool AR_GL_CV_Tester::initCV()
       cout << "Run calibration (in ../calibrate/) first!" << endl;
       return false;
   }
+  cout << "intrinsics " << m_intrinsics << endl;
   //open the camera
   bool result = m_capture.open(-1);
   if (! result){
@@ -110,8 +146,8 @@ bool AR_GL_CV_Tester::initCV()
 //do all the OCV processing here
 void AR_GL_CV_Tester::handleMarkerTracking()
 {
-    Scalar green(0, 255, 0);
-  //clear containers
+  Scalar green(0, 255, 0);
+  //clear containers!!!!!!!!!!!!!!
   m_contours.clear();
   m_squares.clear();
   //convert to greyscale
@@ -144,15 +180,14 @@ void AR_GL_CV_Tester::handleMarkerTracking()
     Mat objectPointsMat(objectPoints);
     cout << "objectPointsMat: " << objectPointsMat.rows << ", " << objectPointsMat.cols << endl;
     cout << "squares[0]: " << m_squares[0] << endl;
-    //Mat rvec;
-    //Mat tvec;
     solvePnP(objectPointsMat, m_squares[0], m_intrinsics, m_distortion, m_rvec, m_tvec);
 
     cout << "rvec = " << m_rvec << endl;
     cout << "tvec = " << m_tvec << endl;
 
-    /*
-    Mat rotation, viewMatrix(4, 4, CV_64F);
+    //create matrices for OpenGL
+    Mat rotation;
+    Mat viewMatrix = Mat::zeros(4, 4, CV_64F);
     Rodrigues(m_rvec, rotation);
     for(unsigned int row=0; row<3; ++row)
     {
@@ -166,16 +201,11 @@ void AR_GL_CV_Tester::handleMarkerTracking()
     viewMatrix.at<double>(3, 3) = 1.0f;
     m_glViewMatrix = Mat::zeros(4, 4, CV_64F);
     transpose(viewMatrix , m_glViewMatrix);
-    */
+    //comp our green poly on the Img
     drawQuad(m_img, m_squares[0], green);
   }
 
 
-
-  glBindTexture(GL_TEXTURE_2D, textureID);
-  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); // scale linearly when image bigger than texture
-  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); // scale linearly when image smalled than texture
-  glTexImage2D(GL_TEXTURE_2D, 0, 3, m_img.cols, m_img.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, m_img.data);
 
 }
 
@@ -184,7 +214,7 @@ void AR_GL_CV_Tester::captureProcessWebcam()
 
   if (m_capture.read(m_img))
   {
-    cout << "read OK" << endl;
+    //cout << "read OK" << endl;
   }
   else
   {
@@ -196,11 +226,26 @@ void AR_GL_CV_Tester::captureProcessWebcam()
 void AR_GL_CV_Tester::display(void)
 {
 
+
     //Clear all pixels
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     glLoadIdentity();
 
+    //glPushMatrix();
     //draw BKGRND
+    Mat flipped;
+    transpose(m_img, flipped);
+    flip(flipped, flipped, 1);
+    //take the video and create BKGRND texture for OpenGL
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); // scale linearly when image bigger than texture
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); // scale linearly when image smalled than texture
+    glTexImage2D(GL_TEXTURE_2D, 0, 3, flipped.cols, flipped.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, flipped.data);
+
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glPushMatrix();
+    //glTranslatef(0.0,0.0,-10.0);
+
     glBegin(GL_QUADS);
 
     float x = 1;
@@ -211,18 +256,19 @@ void AR_GL_CV_Tester::display(void)
     glTexCoord2f(1.0f, 1.0f);glVertex3f( x, y, 0.0f);
     glTexCoord2f(1.0f, 0.0f);glVertex3f(-x, y, 0.0f);
 
-    //draw cube
     glEnd();
+    glPopMatrix();
 
-
-    glPushMatrix();
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadMatrixd(&m_glViewMatrix.at<double>(0, 0));
 
     glBindTexture(GL_TEXTURE_2D, textureID2);
+    glMatrixMode(GL_MODELVIEW);
+        glRotatef(45,1,0,0);
+    glPushMatrix();
+    glLoadMatrixd(&m_glViewMatrix.at<double>(0, 0));
+
     glutSolidTeapot(size);
 
+    glPopMatrix();
     // Don't wait start processing buffered OpenGL routines
     glFlush();
 
@@ -262,121 +308,27 @@ int main( int argc, char** argv ){
     return -1;
   }
 
-  //namedWindow("theWindow", CV_WINDOW_AUTOSIZE);
-  Mat test;
-  //test = ar.getImg();
-  while(1)
-  {
-    ar.captureProcessWebcam();
-    ar.handleMarkerTracking();
-    imshow("theWindow", ar.getImg() );
-    if (waitKey(20) == 27) {return 0;}
-  }
 
-  /*
   glutInit(&argc, argv);
   //set display mode
-  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
   //Set the window size
-  glutInitWindowSize(250,250);
+  glutInitWindowSize(1280,720);
   //Set the window position
   glutInitWindowPosition(100,100);
   //Create the window
   glutCreateWindow("A Simple OpenGL Windows Application with GLUT");
 
-  //Call init (initialise GLUT
   result = ar.initGL();
   if (!result)
   {
     return -1;
   }
-  */
-  //init CV - load calibration
+
+  glutDisplayFunc(oglDraw);
+  glutIdleFunc(oglIdle);
+  glutMainLoop();
 
 
-  //glutDisplayFunc(oglDraw);
-  //glutIdleFunc(oglIdle);
-  //glutMainLoop();
-/*
-  Scalar red(255, 0, 0);
-  Scalar green(0, 255, 0);
-  Scalar blue(0, 0, 255);
-
-
-
-
-
-//Initiate camera
-
-VideoCapture cap(-1);
-Mat img;
-
-namedWindow("theWindow", CV_WINDOW_AUTOSIZE);
-
-while(true){
-
-//if can't read from camera, program dies
-
-  if (!cap.read(img)) { return -1; }
-
-  //convert to greyscale
-  Mat grayImage;
-  cvtColor(img, grayImage, CV_RGB2GRAY);
-  //blur
-  Mat blurredImage;
-  blur(grayImage, blurredImage, Size(5, 5));
-  //threshold
-  Mat threshImage;
-  threshold(blurredImage, threshImage, 128.0, 255.0, THRESH_OTSU);
-
-  std::vector<std::vector<Point> > contours;
-  findContours(threshImage, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-
-  Scalar color(0, 255, 0);
-
-  //fin dthe poly of the square
-  std::vector<Mat> squares;
-  for (auto contour : contours) {
-      vector<Point> approx;
-      approxPolyDP(contour, approx, arcLength(Mat(contour), true)*0.02, true);
-      if( approx.size() == 4 &&
-          fabs(contourArea(Mat(approx))) > 1000 &&
-          isContourConvex(Mat(approx)) )
-      {
-          Mat squareMat;
-          Mat(approx).convertTo(squareMat, CV_32FC3);
-          squares.push_back(squareMat);
-      }
-  }
-
-  if (squares.size() > 0) {
-    vector<Point3f> objectPoints = {Point3f(-1, -1, 0), Point3f(-1, 1, 0), Point3f(1, 1, 0), Point3f(1, -1, 0)};
-    Mat objectPointsMat(objectPoints);
-    cout << "objectPointsMat: " << objectPointsMat.rows << ", " << objectPointsMat.cols << endl;
-    cout << "squares[0]: " << squares[0] << endl;
-    Mat rvec;
-    Mat tvec;
-    solvePnP(objectPointsMat, squares[0], intrinsics, distortion, rvec, tvec);
-
-    cout << "rvec = " << rvec << endl;
-    cout << "tvec = " << tvec << endl;
-
-    drawQuad(img, squares[0], green);
-
-    //draw a calibrated line on the marker
-    vector<Point3f> line3d = {{0, 0, 0}, {0, 0, 1}};
-    vector<Point2f> line2d;
-    projectPoints(line3d, rvec, tvec, intrinsics, distortion, line2d);
-    cout << "line2d = " << line2d << endl;
-    line(img, line2d[0], line2d[1], red);
-}
-
-  //drawContours(img, squares, -1, color);
-  imshow("theWindow", img );
-
-  if (waitKey(20) == 27) {return 0;}
-
-}
-*/
-return 0;
+  return 0;
 }
